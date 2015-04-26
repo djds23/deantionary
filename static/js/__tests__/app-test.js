a21edef099cefc5a39288ac19f4d2c62815192d6
Dean Silfen <dean.silfen@gmail.com>
2015-04-26 16:02:55 -0400

    Start writing client side unit tests

---
+++ static/js/__tests__/app-test.js
@@ -0,0 +1,22 @@
+import React from 'react/addons';
+import {WordDefinition} from '../app';
+
+const TestUtils = React.addons.TestUtils;
+const FakeLookUp = {
+	word: 'Test',
+	definition: 'To determine a status of geniune',
+	suggestions: []
+}
+
+jest.dontMock('../app.jsx');
+describe('app.jsx', () => {
+	it('Test WordDefinition', () => { 
+		const worddefinition = TestUtils.renderIntoDocument(
+		  <WordDefinition LookUpObject={FakeLookUp} />
+		);
+		const label =  TestUtils.findRenderedDOMComponentWithTag(
+		  worddefinition, 'h5'
+		);
+		expect(React.findDOMNode(label).textContent).toEqual('Test');
+	})
+});
